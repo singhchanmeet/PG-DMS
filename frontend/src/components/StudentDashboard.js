daabd8901a2fd129a5682f0f212ff0326ed66f68
Aman <amansharma12607@gmail.com>
2023-09-19 19:40:25 +0530

    Resolved conflicts

--- frontend/src/components/StudentDashboard.js
+++ frontend/src/components/StudentDashboard.js
@@ -1,9 +1,23 @@
-import React from 'react'
+import React from 'react';
+import DissertationList from './DissertationList'; // Adjust the import path as needed
+import DissertationDetail from './DissertationDetail'; // Adjust the import path as needed
+import DissertationForm from './DissertationForm'; // Adjust the import path as needed
 
 const StudentDashboard = () => {
   return (
-    <div>StudentDashboard</div>
-  )
+    <div>
+      <h2>Student Dashboard</h2>
+
+      {/* Render the DissertationList component to display a list of dissertations */}
+      <DissertationList />
+
+      {/* Render the DissertationDetail component to display details of a dissertation */}
+      <DissertationDetail />
+
+      {/* Render the DissertationForm component to create or edit dissertations */}
+      <DissertationForm />
+    </div>
+  );
 }
 
-export default StudentDashboard
+export default StudentDashboard;
